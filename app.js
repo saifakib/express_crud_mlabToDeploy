ce4cf4f74d1b95fe6dc6bd873f46468b5240fe0e
saifakib <mdsaifakib@gmail.com>
2018-12-10 21:34:56 +0600

    initial commit

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ const users = require('./routes/users');
 
 // Passport Config
 require('./config/passport')(passport);
-//DB config
-const db = require('./config/database');
+/*//DB config
+const db = require('./config/database');*/
 // Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
 // Connect to mongoose
-mongoose.connect(db.mongoURI, {
+mongoose.connect('mongodb://localhost/vidjot-dev', {
   useMongoClient: true
 })
   .then(() => console.log('MongoDB Connected...'))
--- config/database.js
+++
@@ -1,9 +0,0 @@
-if(process.env.NODE_ENV === 'production'){
-    module.exports = {
-        mongoURI : 'mongodb://saifakib:akib43215@ds123834.mlab.com:23834/vidjot-dev'
-    }
-}else {
-    module.exports = {
-        mongoURI: 'mongodb://localhost/vidjot-dev'
-    }
-}
